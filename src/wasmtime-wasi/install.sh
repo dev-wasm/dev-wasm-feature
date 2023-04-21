dac6866753a510e5c9e0c7b34c1e7162fd707d70
Brendan Burns <bburns@microsoft.com>
2023-04-21 13:18:24 -0700

    Update install.sh

--- src/wasmtime-wasi/install.sh
+++ src/wasmtime-wasi/install.sh
@@ -3,6 +3,7 @@ set -e
 
 WASI_VERSION="${VERSION:-"20"}"
 WASI_VERSION_FULL="${WASI_VERSION}.0"
+WIT_VERSION="${WIT_VERSION:-"0.4.0"}"
 WASI_LOCATION=${LOCATION:-"/usr/local/lib"}
 
 # TODO: Support Windows someday?
@@ -26,6 +27,14 @@ curl https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_V
 # Install to location
 tar -C ${WASI_LOCATION} -xvf ${FILE}
 rm ${FILE}
+
+# Install wit-bindgen
+curl https://github.com/bytecodealliance/wit-bindgen/releases/download/wit-bindgen-cli-${WIT_VERSION}/wit-bindgen-v${WIT_VERSION}-x86_64-linux.tar.gz -L --output wit-bindgen-v${WIT_VERSION}-x86_64-linux.tar.gz
+
+# Install to location
+tar -xvzf wit-bindgen-v${WIT_VERSION}-x86_64-linux.tar.gz
+cp wit-bindgen-v${WIT_VERSION}-x86_64-linux/wit-bindgen ${LOCATION}/wit-bindgen
+rm -r wit-bindgen-v${WIT_VERSION}-x86_64-linux*
 
 # Install lighttpd for web serving
 apt install -y -qq lighttpd
